df44b9c5fb82a1527abe7fd70518f4fc677da9ca
Chanse Campbell <Chanse@Chanses-MacBook-Pro.local>
2016-03-02 10:38:49 +0000

    Tried to commit the entire directory so I can push it as I'm currently having errors.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,12 +1,13 @@
+
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
 'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -25,31 +26,95 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+   return (move || getInput());
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+   return (move || randomPlay());
 }
+
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    playerMove = getPlayerMove();
+    computerMove = getComputerMove();
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    return winner;
+    if (playerMove === computerMove) {
+            winner = 'tie';
+             console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("It's a tie!");
+        }
+        else if (playerMove === "rock" && computerMove === "scissors") {
+            winner = 'player';
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Player wins!");
+        }
+        else if (playerMove === "paper" && computerMove === "rock") {
+            winner = 'player';
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Player wins!");
+        }
+        else if (playerMove === "scissors" && computerMove === "paper") {
+            winner = 'player';
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Player wins!");
+        }
+
+        else if (computerMove === "rock" && playerMove === "scissors") {
+           winner = 'computer';
+           console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Computer wins!");
+        }
+        else if (computerMove === "paper" && playerMove === "rock") {
+          winner = 'computer';
+          console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Computer wins!");
+        }
+        else if (computerMove === "scissors" && playerMove === "paper") {
+           winner = 'computer';
+           console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '.');
+            console.log("Computer wins!");
+        }
+        return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+    alert("Let's play a game.");
+    alert("Please choose either 'rock', 'paper', or 'scissors'. You may need to do this up to 10 times.");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    
+    while (playerWins < 5 && computerWins < 5) {
+    // Gets the players and computers last move
+    /// Below line is currently causing issues, entering randomPlay() always results in Player chose 'null'.
+    
+        var playerMove;
+        var computerMove;
+    var winner = getWinner(playerMove, computerMove);
+
+        if (winner === "player") {
+        playerWins ++;
+}       else if (winner === "computer") {
+        computerWins ++;
+}       else {
+      winner = "tie";
+}
+    }
+    
+    console.log('Player won ' + playerWins + ' games, whilst Computer won ' + computerWins + ' games.');
+    
+    if (playerWins > computerWins) {
+        console.log('Congratulations champ!');
+    } else 
+        console.log('Better luck next time rookie!');
 }
 
+playToFive();
